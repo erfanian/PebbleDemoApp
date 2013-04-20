02fd9e19e0325c50e3665548bcd0f121502f66e4
Eric Erfanian <eric.erfanian@gmail.com>
2013-04-20 16:05:04 -0500

    add RESOURCE_ID_IMAGE_MENU_ICON

--- src/PebbleDemoApp.c
+++ src/PebbleDemoApp.c
@@ -7,7 +7,7 @@
 PBL_APP_INFO(MY_UUID,
              "Demo App", "Eric Erfanian",
              1, 0, /* App version */
-             DEFAULT_MENU_ICON,
+             RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
 Window window;
