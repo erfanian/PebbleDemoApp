8cd4b17ea032be90adb4509cbab32048326b1913
Eric Erfanian <eric.erfanian@gmail.com>
2013-04-20 15:58:06 -0500

    add simple text function and a layer to draw it on

--- src/PebbleDemoApp.c
+++ src/PebbleDemoApp.c
@@ -5,13 +5,28 @@
 
 #define MY_UUID { 0x60, 0x62, 0xDE, 0x83, 0x46, 0x49, 0x46, 0xBF, 0x9A, 0xD1, 0xE5, 0xE8, 0xAB, 0x1F, 0xA3, 0xD0 }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "Demo App", "Eric Erfanian",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
 Window window;
 
+Layer layer;
+
+void update_layer_callback(Layer *me, GContext* ctx) {
+  (void)me;
+
+  graphics_context_set_text_color(ctx, GColorBlack);
+
+  graphics_text_draw(ctx,
+		     "Hello World.",
+		     fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD),
+		     GRect(5, 5, 200, 200),
+		     GTextOverflowModeWordWrap,
+		     GTextAlignmentLeft,
+		     NULL);
+}
 
 void handle_init(AppContextRef ctx) {
   (void)ctx;
